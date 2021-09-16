c32d08ba3683fbc639546d1ded2358307780da01
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2021-09-16 15:00:13 -0500

    logo added with animation

---
+++ src/header.js
@@ -0,0 +1,13 @@
+function createHeader() {
+    const header = document.createElement('header');
+    const logo = document.createElement('img');
+    logo.src = "/src/img/mdd-logo-w-text.jpeg"
+    logo.alt = "Modern Day Logo, a neutral debate platform on science, religion, and politics"
+    logo.classList.add('animate__animated', 'animate__zoomInDown')
+    logo.id = "header-logo"
+    header.appendChild(logo)
+
+    return header
+}
+
+export default createHeader
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./main.css";
+import "animate.css"
+import createHeader from "./header.js";
+
+const body = document.querySelector('body')
+body.appendChild(createHeader())
