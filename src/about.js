0f27d1d16865a6e02b8c11b13a60a2786dbc6cd7
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2021-09-20 15:22:40 -0500

    about-section-completed

--- src/about.js
+++ src/about.js
@@ -1,8 +1,16 @@
 function createAbout () {
     const aboutSec = document.createElement('section')
+    aboutSec.id = "about-text"
+
+    const aboutSecHead = document.createElement('h2')
+    aboutSecHead.textContent = "Modern-Day Debate's story"
+    aboutSecHead.classList.add('center-text')
+
     const aboutText = document.createElement('p')
-    aboutText.textContent = "Modern-Day Debate's story: My name is James and I'm a doctoral student in psychology in CO, US. In my own academic journey, some academics (though to be fair, not all) have become closed to controversial people or topics and actually want to exclude people that question the status quo. Thus, my goal is to provide a non-partisan and truly-tolerant debate platform that welcomes everyone."
-    aboutText.id = "about-text"
+    aboutText.textContent = "My name is James and I'm a doctoral student in psychology in CO, US. In my own academic journey, some academics (though to be fair, not all) have become closed to controversial people or topics and actually want to exclude people that question the status quo. Thus, my goal is to provide a non-partisan and truly-tolerant debate platform that welcomes everyone."
+    aboutText.classList.add('center-text')
+    
+    aboutSec.appendChild(aboutSecHead)
     aboutSec.appendChild(aboutText);
 
     return aboutSec
@@ -11,6 +19,7 @@ function createAbout () {
 function appearOnScroll(item) {
   const options = {
     threshold: 0,
+    rootMargin: '50px'
     }
 
     const observer = 
